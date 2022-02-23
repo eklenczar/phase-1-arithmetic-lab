0cfb43f2da6dcf370bd1edbd7e5c626800d7b8fa
Elizabeth Klenczar <eklenczar@gmail.com>
2022-02-22 20:36:37 -0500

    build and burn 1

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const num1 = 31;
-const num2 = 2;
-const multiply = num1 * num2;
+// Create a variable called multiply set to an equation that will multiply 
+// the variables num1 and num2; the result of the multiplication should be 62.
 
-const random = Math.ceil(Math.random() * 10) + 1
+const num1 = 1
+const num2 = 62
+const multiply = num1 * num2
 
-const num3 = 10;
-const num4 = 6;
-const mod = num3 % num4;
+// Create a variable called random that will generate a random integer greater 
+// than 0.
 
-const max = Math.max(1, 2, 3, 4, 5, 20);
+const random = Math.floor(Math.random() * 10) + 1
+
+// Create a variable called mod set to an equation that will calculate the 
+// remainder of dividing variable num3 by num4; the remainder should be 4.
+
+const num3 = 10
+const num4 = 6
+const mod = num3 % num4
+
+// Create a variable called max that finds the highest number in a set; the 
+// value returned should be 20.
+
+const max = Math.max(5, 10, 15, 20)
